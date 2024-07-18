a00ef72b25b932f95c75ba71236744f3ad139f06
jdlee726 <jdlee726@gmail.com>
2024-07-18 03:37:50 +0000

    fix

--- alphatory-contexts/coin/coin-containers/coin-exchange/coin-exchange-components/coin-exchange-nasdaq/coin-exchange-nasdaq-manager/src/main/java/alphatory/coin/exchange/nasdaq/manager/NasdaqApiManager.java
+++ alphatory-contexts/coin/coin-containers/coin-exchange/coin-exchange-components/coin-exchange-nasdaq/coin-exchange-nasdaq-manager/src/main/java/alphatory/coin/exchange/nasdaq/manager/NasdaqApiManager.java
@@ -24,7 +24,15 @@ public class NasdaqApiManager {
         this.nasdaqApi = MyRetropitApiServiceGenerator.of("https://api.nasdaq.com")
                 .createService(
                         NasdaqApi.class,
-                        new GzipResponseInterceptor()
+                        chain -> chain.proceed(chain.request().newBuilder()
+                                // .header("Host","api.nasdaq.com")
+                                .header("Accept", "*/*")
+                                // .header("Accept-Encoding", "deflate, gzip, br, zstd")
+                                .header("User-Agent", "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:128.0) Gecko/20100101 Firefox/128.0")
+                                // .header("User-Agent", "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36")
+                                .header("Accept-Language", "en-US,en;q=0.5'")
+                                .build()
+                        )
                 );
     }
 
